cb8562fb128b68f64e3ed32e4c65f10d1f6254e3
dgarrido05 <dgarrido05@gmail.com>
2017-05-18 17:49:28 -0400

    Add files via upload

---
+++ .NET/cesfam/Medico/informeStock.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace cesfam.Medico
+{
+    public partial class informeStock : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnCerrar_Click(object sender, EventArgs e)
+        {
+            Session["usu_id"] = null;
+            Response.Redirect("~/Cesfam.aspx");
+        }
+
+    }
+}
